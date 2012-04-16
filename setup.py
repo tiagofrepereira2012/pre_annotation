8bb1073ef476931ea7d19733c7cc0b0f1c4e0c21
Andre Anjos <andre.dos.anjos@gmail.com>
2012-04-16 08:36:19 +0200

    Slimmed-down example, after transforming Bob into an egg compatible package

--- setup.py
+++ setup.py
@@ -1,40 +1,56 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.anjos@idiap.ch>
+# Mon 16 Apr 08:18:08 2012 CEST
+
+# This file contains the python (distutils/setuptools) instructions so your
+# package can be installed on **any** host system. It defines some basic
+# information like the package name for instance, or its homepage.
+#
+# It also defines which other packages this python package depends on and that
+# are required for this package's operation. The python subsystem will make
+# sure all dependent packages are installed or will install them for you upon
+# the installation of this package.
+#
+# The 'buildout' system we use here will go further and wrap this package in
+# such a way to create an isolated python working environment. Buildout will
+# make sure that dependencies which are not yet installed do get installed, but
+# **without** requiring adminstrative privileges on the host system. This
+# allows you to test your package with new python dependencies w/o requiring
+# administrative interventions.
+
 from setuptools import setup, find_packages
 
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
 setup(
 
-    # This is the basic information about your project. Feel free to modify all
-    # this before releasing code publicly.
+    # This is the basic information about your project. Modify all this
+    # information before releasing code publicly.
     name='bob.project.example',
     version='0.1',
     description='Example for using Bob inside a buildout project',
     url='http://github.com/idiap/bob.project.example',
     license='LICENSE.txt',
-    author='Andre Anjos',
-    author_email='andre.anjos@idiap.ch',
+    author_email='Andre Anjos <andre.anjos@idiap.ch>',
 
     # If you have a better, long description of your package, place it on the
-    # 'doc' directory and then hook it here, uncommenting the following line
-    #long_description=open('doc/long-description.rst').read(),
+    # 'doc' directory and then hook it here
+    long_description=open('doc/long-description.rst').read(),
 
     # This line is required for any distutils based packaging.
     packages=find_packages(),
 
     # This line defines which packages should be installed when you "install"
-    # this package. When using "buildout", this refers to "local" installation
-    # of the package. So, don't worry. You won't need adminstrative privileges
-    # when using buildout.
-    #
-    # Anyhow, you need to put here the "required" material you will want to run
-    # against. Buildout will not bother about python packages that are already
-    # installed. It will locally install those python modules which are missing.
+    # this package. All packages that are mentioned here, but are not installed
+    # on the current system will be installed locally and only visible to the
+    # scripts of this package. Don't worry - You won't need adminstrative
+    # privileges when using buildout.
     install_requires=[
-        #"bob",      # base signal proc./machine learning library
+        "bob",      # base signal proc./machine learning library
 
         # here are some more examples you could include:
-
         #"argparse", # better option parsing
-        #"ipython",  # better prompt than the stock python
-        #"ipdb",     # better debugging support with syntax highlighting
     ],
 
     # This entry defines which scripts you will have inside the 'bin' directory
@@ -42,10 +58,15 @@ setup(
     # entry under 'console_scripts' is like this:
     #   script-name-at-bin-directory = module.at.your.library:function
     #
-    # The module.at.your.library is the python file within your library.
+    # The module.at.your.library is the python file within your library, using
+    # the python syntax for directories (i.e., a '.' instead of '/' or '\').
+    # This syntax also omits the '.py' extension of the filename. So, a file
+    # installed under 'example/foo.py' that contains a function which
+    # implements the 'main()' function of particular script you want to have
+    # should be referred as 'example.foo:main'.
     #
-    # In this example we will create a single program that will print the
-    # version of bob.
+    # In this simple example we will create a single program that will print
+    # the version of bob.
     entry_points={
       'console_scripts': [
         'test.py = example.test:main',
