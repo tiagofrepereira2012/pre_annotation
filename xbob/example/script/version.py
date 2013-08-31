345ea9f312efb81ba48f55e9a86ba1225339ba76
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-31 15:50:27 +0200

    python 3 compatibility + add copyright

--- xbob/example/script/version.py
+++ xbob/example/script/version.py
@@ -2,6 +2,20 @@
 # vim: set fileencoding=utf-8 :
 # Andre Anjos <andre.dos.anjos@gmail.com>
 # Sun 15 Apr 14:01:39 2012 
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Prints the version of bob and exits
 """
@@ -14,10 +28,10 @@ def main():
   this = packages[0]
   deps = packages[1:]
 
-  print "The installed version of %s is %s" % (this.key, this.version)
-  print "%s is installed at: %s" % (this.key, this.location)
-  print "%s depends on the following Python packages:" % (this.key,)
+  print("The installed version of %s is %s" % (this.key, this.version))
+  print("%s is installed at: %s" % (this.key, this.location)
+  print("%s depends on the following Python packages:" % (this.key,))
   for d in deps:
-    print " * %s: %s (%s)" % (d.key, d.version, d.location)
+    print(" * %s: %s (%s)" % (d.key, d.version, d.location))
 
   return 0
