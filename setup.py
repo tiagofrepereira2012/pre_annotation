6387603b5ee715a36b5f80db6de3da0066841a0a
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-31 15:49:13 +0200

    Add copyright in MANIFEST; add keywords

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@
 # vim: set fileencoding=utf-8 :
 # Andre Anjos <andre.anjos@idiap.ch>
 # Mon 16 Apr 08:18:08 2012 CEST
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 # This file contains the python (distutils/setuptools) instructions so your
 # package can be installed on **any** host system. It defines some basic
@@ -35,6 +49,7 @@ setup(
     license='GPLv3',
     author='Andre Anjos',
     author_email='andre.anjos@idiap.ch',
+    keywords='bob, xbob',
 
     # If you have a better, long description of your package, place it on the
     # 'doc' directory and then hook it here
